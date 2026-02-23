80d0874760d0ec1719e0cc4fba7b1f42e287b2eb
jamuna7318 <jamunachelladurai7@gmail.com>
2026-02-23 11:36:56 +0530

    Initial commit

---
+++ backend/src/routes/notesRoutes.js
@@ -0,0 +1,15 @@
+import express from "express"
+import { createNote,deleteNote,getAllNotes,getNotesById,updateNote } from "../controller/notescontroller.js";
+const router = express.Router();
+
+router.get("/" ,getAllNotes);
+
+router.get("/:id" ,getNotesById);
+
+router.post("/" ,createNote);
+
+router.put("/:id" ,updateNote);
+
+router.delete("/:id" ,deleteNote);
+
+export default router;
---
+++ frontend/src/components/NotesNotFound.jsx
@@ -0,0 +1,10 @@
+const NotesNotFound = () => {
+  return (
+    <div className="text-center py-10">
+      <h2 className="text-xl font-semibold">No Notes Found</h2>
+      <p className="opacity-70">Create your first note </p>
+    </div>
+  );
+};
+
+export default NotesNotFound;
