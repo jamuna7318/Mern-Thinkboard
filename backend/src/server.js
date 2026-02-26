902bb0a29acd363968ae46582fb3ab74d0fe98a3
jamuna7318 <jamunachelladurai7@gmail.com>
2026-02-26 16:11:51 +0530

    prepared for the development

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,27 +1,37 @@
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 dotenv.config();
-
-import express from "express"
+import express from "express";
 import notesRoutes from "./routes/notesRoutes.js";
 import connectDB from "./config/db.js";
 import rateLimiter from "./middleware/rateLimiter.js";
-import cors from "cors"
+import cors from "cors";
+import path from "path";
 const app = express();
+const __dirname = path.resolve();
 
 //middleware
-app.use(cors({
-orgin : "http://localhost:5173",
- credentials: true,
-}))
+if (process.env.NODE_ENV !== "production"){
+ app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  }),
+);
+}
 app.use(express.json());
+
 app.use(rateLimiter);
 
+//routes
 app.use("/api/notes", notesRoutes);
-
-connectDB().then( ()=>{
-
-
-app.listen(5001, ()=> {
-  console.log("Server started on PORT: 5001");
-})
-})
+if(process.env.NODE_ENV === "production"){
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
+connectDB().then(() => {
+  app.listen(5001, () => {
+    console.log("Server is running on port 5001");
+  });
+});
